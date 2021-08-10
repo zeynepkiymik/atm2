fcf15cde02ee25d70176dc7423c0ba83cf67d6e4
zeynepkiymik <33024264+zeynepkiymik@users.noreply.github.com>
2021-08-10 16:19:26 +0300

    Create AtmController.php

---
+++ app/Http/Controllers/AtmController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Session;
+
+
+class AtmController extends Controller
+{
+    public function paraCek() {
+        return view('paracek');
+    }
+    public function postParaCek(Request $request) {
+         
+        
+        $yeniBakiye = Session::get('bakiye') -  $request->cekilen_para;
+     
+        $request->session()->put('bakiye', $yeniBakiye);
+
+        return redirect()->route('girissayfasi');
+    }
+
+    public function paraEkle() {
+        return view('paraekle');
+    }
+
+    public function sifremiUnuttum() {
+        return view('sifremiUnuttum');
+    }
+
+    public function girisSayfasi(Request $request) {
+        if ( !Session::has('bakiye'))
+        $request->session()->put('bakiye', 10000);
+
+        return view('girissayfasi');
+    }
+    public function index() {
+        return view('atm');
+    }
+}
+
+//AuthController
