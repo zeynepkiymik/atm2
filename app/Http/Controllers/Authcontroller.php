890ee9fbc784dcecf8e8c22a4bbdf8ad9c98e89a
zeynepkiymik <33024264+zeynepkiymik@users.noreply.github.com>
2021-09-30 11:39:00 +0300

    Update Authcontroller.php

--- app/Http/Controllers/Authcontroller.php
+++ app/Http/Controllers/Authcontroller.php
@@ -9,5 +9,23 @@ class Authcontroller extends Controller
     public function cikis() {
         return view('cikis');
     }
+    public function postcikis (Request $request){
+        return redirect()->route('index');   
+    }
+
+
 }
 
+
+/*public function paraCek() {
+    return view('paracek');
+}
+public function postParaCek(Request $request) {
+     
+    
+    $yeniBakiye = Session::get('bakiye') -  $request->cekilen_para;
+ 
+    $request->session()->put('bakiye', $yeniBakiye);
+
+    return redirect()->route('girissayfasi');
+}
